6f0e249580de8b8f27198673b7108ed1a67580fb
Firstname Lastname <your_email@youremail.com>
2015-02-21 22:20:12 +0900

    fix #1

---
+++ test/_1testcode/_2/DateUtilTest.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package _1testcode._2;
+
+import _1testcode._1.*;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import org.junit.Test;
+
+public class DateUtilTest {
+
+    @Test
+    public void _今日20150202は経過年数が０() {
+
+    }
+
+    @Test
+    public void _明日20150203は経過年数が０() {
+
+    }
+    
+    @Test
+    public void _昨日20150201は経過年数が０() {
+
+    }
+
+    @Test
+    public void _去年20140202は経過年数が１() {
+
+    }
+
+}
